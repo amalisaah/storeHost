8aab8a8a27cb16fbbe95c504ba37a577962c8138
amalisaah <saah.asiedu@amalitech.org>
2022-12-09 14:36:13 +0000

    initializing repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <h1 className="text-3xl font-bold text-red-600">
+        hello
+      </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
